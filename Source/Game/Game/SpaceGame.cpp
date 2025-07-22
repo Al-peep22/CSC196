5b855a69c6d897775f06b7b30d78b51d25bcacd9
Al-peep22 <awaldsmith@student.neumont.edu>
2025-07-22 14:55:23 -0600

    got some missing info for SpaceGame.h/.cpp and Game.h

---
+++ Source/Game/Game/SpaceGame.cpp
@@ -0,0 +1,74 @@
+#include "SpaceGame.h"
+
+#include "Core/Random.h"
+#include "Framework/Scene.h"
+#include "Math/Vector2.h"
+#include "Player.h"
+//#include "Renderer/Font.h"
+#include "Renderer/Model.h"
+#include "Renderer/Renderer.h"
+//#include "Renderer/Text.h"
+#include "Engine.h"
+
+#include <vector>
+
+bool SpaceGame::Initialize() {
+    scene = std::make_unique<viper::Scene>();
+
+    std::vector<viper::vec2> ship_points{
+            { 6, 0 },
+            { -2, -4 },
+            { -2, -6 },
+            { -4, -6 },
+            { -4, -2 },
+            { 4, 0 },
+            { -4, 2 },
+            { -4, 6 },
+            { -2, 6 },
+            { -2, 3 },
+            { 6, 0 },
+            { -3, 0 },
+            { -2, -4 },
+            { -3, 0 },
+            { -2, 3 }
+    };
+
+    //viper::Model ship_Model{ ship_points, viper::vec3{ 255, 255, 255} };
+    std::shared_ptr<viper::Model> ship_model = std::make_shared<viper::Model>(ship_points, viper::vec3{ 96, 255, 41 });
+
+    viper::Transform transform{ viper::vec2{ viper::GetEngine().GetRenderer().GetWidth() * 0.5f , viper::GetEngine().GetRenderer().GetHeight() * 0.5f}, 0, 2 };
+    std::unique_ptr<Player> player = std::make_unique<Player>(transform, ship_model);
+    scene->AddActor(std::move(player));
+
+    // SAVING CODE FOR ENEMY CODE
+    /*for (int i = 0; i < 10; i++) {
+        viper::Transform transform{ viper::vec2{viper::random::getRandomFloat() * 1280, viper::random::getRandomFloat() * 1024}, 0, 2 };
+        std::unique_ptr<Player> player = std::make_unique<Player>(transform, model);
+        scene->AddActor(std::move(player));
+    }*/
+
+    // FONT CREATION
+    /*std::unique_ptr<viper::Font> font = std::make_unique<viper::Font>();
+    font->Load("ArcadeClassic.ttf", 20);*/
+
+    // TEXT CREATION
+    /*_text = std::make_unique<viper::Text>(font);
+    _text->Create(viper::GetEngine().GetRenderer(), "Hello World", viper::vec3{ 1, 1, 1 });*/
+
+    return true;
+}
+
+void SpaceGame::Update() {
+    scene->Update(viper::GetEngine().GetTime().GetDeltaTime());
+}
+
+void SpaceGame::Draw() {
+    scene->Draw(viper::GetEngine().GetRenderer());
+
+    //// DRAW TEXT
+    //_text->Draw(viper::GetEngine().GetRenderer(), 40.0f, 40.0f);
+}
+
+void SpaceGame::Shutdown() {
+
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -10,6 +10,7 @@
 #include "Renderer/Renderer.h"
 #include "Renderer/Model.h"
 #include "Game/Player.h"
+#include "Game/SpaceGame.h"
 #include <fmod.hpp>  
 #include <vector>
 #include <iostream>
@@ -64,6 +65,18 @@ int main(int argc, char* argv[]) {
     //input->Initialize();
     GetEngine().Initialize();
 
+    // Initialize Game
+    std::unique_ptr<SpaceGame> game = std::make_unique<SpaceGame>();
+    game->Initialize();
+
+    //// FONT CREATION
+    //errera::Font* font = new errera::Font();
+    //font->Load("ArcadeClassic.ttf", 20);
+
+    //// TEXT CREATION
+    //errera::Text* text = new errera::Text(font);
+    //text->Create(errera::GetEngine().GetRenderer(), "Hello World", errera::vec3{ 1, 1, 1 });
+
     /*SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = SDL_CreateWindow("SDL3 Project", width, height, 0);
@@ -88,7 +101,7 @@ int main(int argc, char* argv[]) {
     
 
     
-    std::vector<viper::vec2> ship_points{ 
+    /*std::vector<viper::vec2> ship_points{ 
         { 6, 0 },
         { -2, -4 },
         { -2, -6 },
@@ -105,36 +118,36 @@ int main(int argc, char* argv[]) {
         { -3, 0 },
         { -2, 3 }
     };
-    viper::Model ship_Model{ ship_points, viper::vec3{ 255, 255, 255} };
+    viper::Model ship_Model{ ship_points, viper::vec3{ 255, 255, 255} };*/
 
-    std::vector<viper::vec2> sqr_points{ 
-        viper::vec2{-5,-5}, 
-        viper::vec2{5,-5}, 
-        viper::vec2{5,5}, 
-        viper::vec2{-5,5},
-        viper::vec2{-5,-5}
-    };
-    viper::Model sqr_model{ sqr_points, viper::vec3{ 255, 255, 255} };
-    std::shared_ptr<viper::Model> model = std::make_shared<viper::Model>(sqr_points, viper::vec3{ 255, 255, 255 });
-    //viper::Model* model = new Model{ sqr_points, viper::vec3{ 255, 255, 255} };
+    //std::vector<viper::vec2> sqr_points{ 
+    //    viper::vec2{-5,-5}, 
+    //    viper::vec2{5,-5}, 
+    //    viper::vec2{5,5}, 
+    //    viper::vec2{-5,5},
+    //    viper::vec2{-5,-5}
+    //};
+    //viper::Model sqr_model{ sqr_points, viper::vec3{ 255, 255, 255} };
+    //std::shared_ptr<viper::Model> model = std::make_shared<viper::Model>(sqr_points, viper::vec3{ 255, 255, 255 });
+    ////viper::Model* model = new Model{ sqr_points, viper::vec3{ 255, 255, 255} };
 
-    Scene scene;
+    //Scene scene;
 
-    std::vector<viper::vec2> boat_points{
-        viper::vec2{0,2},
-        viper::vec2{0,5},
-        viper::vec2{-3,2},
-        viper::vec2{0,2},
-        viper::vec2{0,1},
-        viper::vec2{-3,1},
-        viper::vec2{-2,-1},
-        viper::vec2{2,-1},
-        viper::vec2{3,1},
-        viper::vec2{0,1}
-    };
-    viper::Model boat_Model{ boat_points, viper::vec3{ 255, 255, 255} };
+    //std::vector<viper::vec2> boat_points{
+    //    viper::vec2{0,2},
+    //    viper::vec2{0,5},
+    //    viper::vec2{-3,2},
+    //    viper::vec2{0,2},
+    //    viper::vec2{0,1},
+    //    viper::vec2{-3,1},
+    //    viper::vec2{-2,-1},
+    //    viper::vec2{2,-1},
+    //    viper::vec2{3,1},
+    //    viper::vec2{0,1}
+    //};
+    //viper::Model boat_Model{ boat_points, viper::vec3{ 255, 255, 255} };
 
-    viper::Transform transform{ {640,512}, 0 , 20 };
+    //viper::Transform transform{ {640,512}, 0 , 20 };
     /*FMOD::System* audio;
     FMOD::System_Create(&audio);
 
@@ -150,26 +163,26 @@ int main(int argc, char* argv[]) {
     
 
 
-    std::vector<std::unique_ptr<viper::Actor>> actors;
-    /*for (int i = 0; i < 10; i++) {
-        viper::Transform m_transform{ {viper::random::getRandomFloat() * GetEngine().GetRenderer().GetWidth(), viper::random::getRandomFloat() * GetEngine().GetRenderer().GetHeight() },(float)viper::random::getRandomInt(360),(float)viper::random::getRandomInt(50) };
-        std::unique_ptr<Player> player = std::make_unique<Player>(m_transform,model);
-        scene.AddActor(std::move(player));
-    }*/
-    viper::Transform ship_transform{ {640,512}, 0 , 20 };
-    std::unique_ptr<Player> player = std::make_unique<Player>(ship_transform, ship_Model);
-    scene.AddActor(std::move(player));
+    //std::vector<std::unique_ptr<viper::Actor>> actors;
+    ///*for (int i = 0; i < 10; i++) {
+    //    viper::Transform m_transform{ {viper::random::getRandomFloat() * GetEngine().GetRenderer().GetWidth(), viper::random::getRandomFloat() * GetEngine().GetRenderer().GetHeight() },(float)viper::random::getRandomInt(360),(float)viper::random::getRandomInt(50) };
+    //    std::unique_ptr<Player> player = std::make_unique<Player>(m_transform,model);
+    //    scene.AddActor(std::move(player));
+    //}*/
+    //viper::Transform ship_transform{ {640,512}, 0 , 20 };
+    //std::unique_ptr<Player> player = std::make_unique<Player>(ship_transform, ship_Model);
+    //scene.AddActor(std::move(player));
     //viper::Actor actor{ transform, model};
 
     // initialize sounds
     //std::vector<FMOD::Sound*> sounds;
 
-    GetEngine().GetAudio().AddSound("bass.wav", "bass");
+    /*GetEngine().GetAudio().AddSound("bass.wav", "bass");
     GetEngine().GetAudio().AddSound("snare.wav", "snare");
     GetEngine().GetAudio().AddSound("open-hat.wav", "openhat");
     GetEngine().GetAudio().AddSound("clap.wav", "clap");
     GetEngine().GetAudio().AddSound("cowbell.wav", "cowbell");
-    GetEngine().GetAudio().AddSound("close-hat.wav", "closehat");
+    GetEngine().GetAudio().AddSound("close-hat.wav", "closehat");*/
 
     //audio->createSound("bass.wav", FMOD_DEFAULT, 0, &sound);
    /* sounds.push_back(sound);
@@ -199,7 +212,7 @@ int main(int argc, char* argv[]) {
     // Define a rectangle
     //SDL_FRect greenSquare{ 270, 190, 200, 200 };
 
-    std::vector<viper::vec2> points;
+    //std::vector<viper::vec2> points;
     //MAIN LOOP
     while (!quit) {
         //time->Tick();
@@ -210,7 +223,7 @@ int main(int argc, char* argv[]) {
         }
 
         GetEngine().Update();
-        //game->Update();
+        game->Update();
 
         if (GetEngine().GetInput().GetKeyPressed(SDL_SCANCODE_ESCAPE)) quit = true;
 
@@ -291,21 +304,24 @@ int main(int argc, char* argv[]) {
             direction = direction.Normalized();
         }*/
         //transform.position += (direction * sqr_speed) * time.GetDeltaTime();
-        for (auto& actor : actors) {
+        /*for (auto& actor : actors) {
             actor->Update(GetEngine().GetTime().GetDeltaTime());
-        }
+        }*/
 
-
-        GetEngine().GetRenderer().SetColor(0, 0, 0);
+        vec3 color{0,0,0};
+        GetEngine().GetRenderer().SetColor(color.x, color.y, color.z);
         GetEngine().GetRenderer().Clear();
 
-        //game->Draw();
+        //// DRAW TEXT
+        //text->Draw(errera::GetEngine().GetRenderer(), 40.0f, 40.0f);
+
+        game->Draw();
 
         //model.Draw(renderer,input.GetMousePosition(),viper::math::halfPi * 0.5f,10.0f);
-        scene.Draw(GetEngine().GetRenderer());
+        /*scene.Draw(GetEngine().GetRenderer());
         for (auto& actor : actors) {
             actor->Draw(GetEngine().GetRenderer());
-        }
+        }*/
 
         //sqr_model.Draw(GetEngine().GetRenderer(), GetEngine().GetInput().GetMousePosition(),GetEngine().GetTime().GetTime(), 10.0f);
         //boat_Model.Draw(GetEngine().GetRenderer(), {640,512}, GetEngine().GetTime().GetTime(), 50.0f);
@@ -385,8 +401,8 @@ int main(int argc, char* argv[]) {
 
     //delete model;
     
-    //game->Shutdown();
     GetEngine().Shutdown();
+    game->Shutdown();
 
     return 0;
 }
